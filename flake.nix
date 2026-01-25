9e7d2c9949619bfa27b6d2cc97581ef9df91b6ac
Spencer Janssen <spencerjanssen@gmail.com>
2026-01-25 15:42:05 -0600

    nix based build

--- flake.nix
+++ flake.nix
@@ -3,16 +3,35 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+    flake-parts.inputs.nixpkgs-lib.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShellNoCC {
-      packages = [
-        nixpkgs.legacyPackages.x86_64-linux.treefmt
-        nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt
-        nixpkgs.legacyPackages.x86_64-linux.haskellPackages.fourmolu
-        nixpkgs.legacyPackages.x86_64-linux.haskellPackages.cabal-fmt
-      ];
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      imports = [ inputs.haskell-flake.flakeModule ];
+      perSystem = { self', system, lib, config, pkgs, ... }: {
+        haskellProjects.default = {
+          basePackages = pkgs.haskell.packages.ghc912;
+          devShell = {
+            tools = hp: {
+              inherit (pkgs)
+                treefmt
+                nixpkgs-fmt
+                ;
+              cabal-fmt = pkgs.haskellPackages.cabal-fmt;
+            };
+          };
+        };
+      };
     };
+  nixConfig = {
+    extra-substituters = [ "https://sjanssen-trusted-publishing.cachix.org" ];
+    extra-trusted-public-keys = [
+      "sjanssen-trusted-publishing.cachix.org-1:p8MLBVXkrJI945XKQefinNic2umUPDiq9dEb5/9M9lM="
+    ];
+    allow-import-from-derivation = "true";
   };
 }
